e35a9a18f6b9a60c3fba87a5fcf2bcfbf0dcfd22
Oriya124578 <oriya.margalit@post.runi.ac.il>
2025-11-14 00:37:51 +0200

    finished 3333

--- LoanCalc.java
+++ LoanCalc.java
@@ -58,9 +58,11 @@ public class LoanCalc {
     // the number of periods (n), and epsilon, the approximation's accuracy
     // Side effect: modifies the class variable iterationCounter.
     public static double bisectionSolver(double loan, double rate, int n, double epsilon) {  
-        iterationCounter = 1; 
+        iterationCounter = 0; // התחל מ-0
         double L = loan / n;
         double H = 2 * L;
+        
+        iterationCounter++; // ספר את בדיקת L הראשונית
         
         while (endBalance(loan, rate, n, H) > 0) {
             iterationCounter++;
