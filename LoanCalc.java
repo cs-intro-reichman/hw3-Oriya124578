5a82eebfc3b650bbbe629e00b690e740e84d9387
Oriya124578 <oriya.margalit@post.runi.ac.il>
2025-11-13 12:45:01 +0200

    finish 2

--- LoanCalc.java
+++ LoanCalc.java
@@ -29,7 +29,16 @@ public class LoanCalc {
 	// interest rate (as a percentage), the number of periods (n), and the periodical payment.
 	private static double endBalance(double loan, double rate, int n, double payment) {	
 		// Replace the following statement with your code
-		return 0;
+
+
+		double balance = loan;
+		double factor = 1 + (rate / 100);
+		for (int i = 0; i < n; i++) {
+			balance = (balance - payment) * factor;
+		}
+
+
+		return balance;
 	}
 	
 	// Uses sequential search to compute an approximation of the periodical payment
@@ -39,7 +48,19 @@ public class LoanCalc {
 	// Side effect: modifies the class variable iterationCounter.
     public static double bruteForceSolver(double loan, double rate, int n, double epsilon) {
 		// Replace the following statement with your code
-		return 0;
+
+		iterationCounter = 0;
+		double guess = loan / n; 
+		
+		while (endBalance(loan, rate, n, guess) > 0){
+			iterationCounter++;
+			guess += epsilon;}
+		
+
+
+		
+
+		return guess;
     }
     
     // Uses bisection search to compute an approximation of the periodical payment 
@@ -49,6 +70,23 @@ public class LoanCalc {
 	// Side effect: modifies the class variable iterationCounter.
     public static double bisectionSolver(double loan, double rate, int n, double epsilon) {  
         // Replace the following statement with your code
-		return 0;
+
+		iterationCounter = 0; 
+		double L = loan / n;
+		double H = 2 * L;
+		while (endBalance(loan, rate, n, H) > 0) {
+			iterationCounter++;
+			H = H * 2;
+		}
+		while (H - L > epsilon) {
+			iterationCounter++;
+			double M = (L + H) / 2;
+			if (endBalance(loan, rate, n, M) > 0) {
+				L = M;
+			} else {
+				H = M;
+			}
+		}
+		return (L + H) / 2;
     }
 }
